dcf2125af495c42dd0de3c307aa1277afe0e4d49
Hanna Kozak <hannawkozak@gmail.com>
2020-05-11 22:41:19 +0100

    Updates

---
+++ precache-manifest.5504494ad3d068c7dde3642fc9c46efe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91532b5ff20f6dae5e534daafbb2272",
+    "url": "/myreads/index.html"
+  },
+  {
+    "revision": "0e84550dfd335c330d1b",
+    "url": "/myreads/static/css/main.15cff7c7.chunk.css"
+  },
+  {
+    "revision": "41d8d9c94f51a8054634",
+    "url": "/myreads/static/js/2.441750bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreads/static/js/2.441750bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e84550dfd335c330d1b",
+    "url": "/myreads/static/js/main.f9fa3010.chunk.js"
+  },
+  {
+    "revision": "6259aadd35d1b500b365",
+    "url": "/myreads/static/js/runtime-main.adac3c9b.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
